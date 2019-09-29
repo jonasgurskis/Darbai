e293132325297a85f2f4fa9540c9f6a4013505c8
jonasgurskis <43638634+jonasgurskis@users.noreply.github.com>
2019-09-30 00:09:33 +0300

    Update Student.java

--- Java2/Student.java
+++ Java2/Student.java
@@ -1,1 +1,30 @@
-
+public class Student {
+    private String Name;
+    private String Lastname;
+    private String Group;
+    public Subject Subjects;
+    public Student (String name, String lastname, String group, String[] subject, String marks, double credits) {
+        Name = name;
+        Lastname = lastname;
+        Group = group;
+        Subjects = new Subject(subject, marks, credits);
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getLastname() {
+        return Lastname;
+    }
+    public String getGroup() {
+        return Group;
+    }
+    public void setName (String newName) {
+        Name = newName;
+    }
+    public void setLastname(String newLastname) {
+        Lastname = newLastname;
+    }
+    public void setGroup(String newGroup) {
+        Group = newGroup;
+    }
+}
